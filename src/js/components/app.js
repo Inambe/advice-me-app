fc96d264fef3ecb82e84c223ff835018181719a0
inambe <inamulhuq44@outlook.com>
2019-02-23 14:44:54 +0500

    basics implemented

--- src/js/components/app.js
+++ src/js/components/app.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import axios from "axios";
+import Footer from './footer';
 
 class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            advice: null
+        }
+    }
+    componentDidMount(){
+        axios.get("https://api.adviceslip.com/advice")
+        .then((res) => {
+            this.setState({
+                advice: res.data.slip.advice.replace(".", "")
+            });
+        }).catch((e) => {
+            console.log(e);
+        });
+    }
     render() {
         return (
-            <div>
-                Advice App
+            <div className="advice-app">
+                <h1 className="advice-app--title">Advice 🎉</h1>
+                <div className="advice-app--wrapper">
+                {this.state.advice ? (
+                    <p
+                        style={{fontSize: this.state.advice.length > 50 ? "1.6rem" : null }}
+                        className="advice-app--advice">
+                    {this.state.advice}
+                    </p>
+                ) : (
+                    <div className="advice-app--loader">Loading ...</div>
+                )}
+                </div>
+                <Footer />
             </div>
         );
     }
---
+++ src/js/components/footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className="footer">
+            <p className="footer--attr">
+                Powered by
+                <a target="_blank" href="https://api.adviceslip.com/">Advice Slip JSON API</a>
+                Designed by
+                <a target="_blank" href="https://github.com/inambe">Inam Ul Huq</a>
+            </p>
+        </div>
+    );
+};
+
+export default Footer;
